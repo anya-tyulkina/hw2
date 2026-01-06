060bbb1c6946f27b934da7725a8f4344a19b1a70
annatulkina <an.tyulkina@mail.ru>
2026-01-06 05:20:23 +0300

    second2

--- src/s2-homeworks/hw01/HW1.tsx
+++ src/s2-homeworks/hw01/HW1.tsx
@@ -25,7 +25,7 @@ export const message0: MessageType = {
     id: 0,
     user: {
         avatar: avatar, // можно менять
-        name: 'ANya',  // можно менять
+        name: 'Some Name',  // можно менять
     },
     message: {
         text: 'some textsome textsome textsome textsome textsome textsome text', // можно менять
