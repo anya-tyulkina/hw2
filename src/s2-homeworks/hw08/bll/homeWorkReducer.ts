3d7556ebe7752c15cc601d0a61dac21bf10a7c85
annatulkina <an.tyulkina@mail.ru>
2026-02-23 16:07:51 +0300

    HW8

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,15 +4,26 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
 
-            return state // need to fix
+            if(action.payload === 'up') {
+                return state.sort((a, b) => {
+                    return a.name < b.name ? -1 : 1 // need to fix
+                })
+            } else if (action.payload === 'down') {
+                return state.sort((a, b) => {
+                    return a.name > b.name ? -1 : 1 // need to fix
+                })
+            }
+
+            return state
+
         }
         case 'check': {
 
-            return state // need to fix
+            return state.filter(i => i.age > 18)
         }
         default:
             return state
