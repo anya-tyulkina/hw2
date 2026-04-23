b0ab9af838226f0ae3ffdd97e4abd88068db2eec
annatulkina <an.tyulkina@mail.ru>
2026-04-23 13:33:39 +0300

    hw 13

--- src/s2-homeworks/hw13/HW13.tsx
+++ src/s2-homeworks/hw13/HW13.tsx
@@ -38,7 +38,7 @@ const HW13 = () => {
             .then((res) => {
                 setCode('Код 200!')
                 setImage(success200)
-                setText('...всё ок) код 200 - обычно означает что скорее всего всё ок)')
+                setText('...всё ок)')
                 setInfo('')
                 // дописать
 
@@ -49,12 +49,12 @@ const HW13 = () => {
                 if (e.response.status === 500) {
                     setCode('Ошибка 500!')
                     setImage(error500)
-                    setText('эмитация ошибки на сервере ошибка 500 - обычно означает что что-то сломалось на сервере, например база данных)')
+                    setText('эмитация ошибки на сервере')
                     setInfo('')
                 } else if (e.response.status === 400) {
                     setCode('Ошибка 400!')
                     setImage(error400)
-                    setText('Ты не отправил success в body вообще! ошибка 400 - обычно означает что скорее всего фронт отправил что-то не то на бэк!')
+                    setText('Ты не отправил success в body вообще!')
                     setInfo('')
                 } else {
                     setCode('Error!')
